bb59a5682dfef50392ec0f790a395dfb639b441d
StewardRighteous <110706656+StewardRighteous@users.noreply.github.com.>
2026-06-06 11:30:06 +0530

    ex 3.1 phone book backend returning persons

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log("Working...")
+const express = require("express");
+const app = express();
+
+let phoneBook = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/api/persons", (req, res) => res.json(phoneBook));
+app.get("/", (req, res) => res.json({ name: "PhoneBook API" }));
+
+const PORT = 3001;
+app.listen(PORT, () => console.log("Server is running "));
