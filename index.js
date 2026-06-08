5f91232c2f57da080e1c9ebb2f7281d6d0925cf5
StewardRighteous <110706656+StewardRighteous@users.noreply.github.com.>
2026-06-08 10:12:27 +0530

    ex 3.8 show data in POST requests

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ const morgan = require("morgan");
 
 const app = express();
 
-app.use(morgan("tiny"));
 app.use(express.json());
+
+morgan.token("data", (req, res) => JSON.stringify(req.body));
+
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :data"),
+);
 
 let phoneBook = [
   {
