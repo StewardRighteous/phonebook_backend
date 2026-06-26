e4c7964f8e9871277e3a172e5adf19b1158ba83f
StewardRighteous <110706656+StewardRighteous@users.noreply.github.com.>
2026-06-26 17:49:49 +0530

    3.20 , 3.21 add validation to number and hosting it online

--- models/person.js
+++ models/person.js
@@ -6,7 +6,14 @@ const personSchema = new mongoose.Schema({
     minLength: 3,
     required: true,
   },
-  number: Number,
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: (v) => /^\d{2,3}-\d{7,8}$/.test(v),
+      message: (props) => `${props.value} is not a valid phone number`,
+    },
+  },
 });
 
 personSchema.set("toJSON", {
